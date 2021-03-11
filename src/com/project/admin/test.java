e87371d265baab3d1051446ddf62bafb4b501ca7
hyeseungcho <jhs9214@gmail.com>
2021-03-11 16:34:21 +0900

    ssacademy
    ssacademy

---
+++ src/com/project/admin/test.java
@@ -0,0 +1,12 @@
+package com.project.admin;
+
+import com.project.student.GetJobInfoCheck;
+
+public class test {
+	public static void main(String[] args) {
+		GetJobInfo cj = new GetJobInfo();
+		cj.menu();
+		
+	}
+
+}
